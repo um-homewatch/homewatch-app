4ebfc1852e0dd6003aff772ae14e1f9c72235045
José Sousa <josesousa9000@gmail.com>
2017-05-26 21:42:31 +0100

    Improve declarations

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,54 +1,56 @@
-/*
-  Declaration files are how the Typescript compiler knows about the type information(or shape) of an object.
-  They're what make intellisense work and make Typescript know all about your code.
-
-  A wildcard module is declared below to allow third party libraries to be used in an app even if they don't
-  provide their own type declarations.
-
-  To learn more about using third party libraries in an Ionic app, check out the docs here:
-  http://ionicframework.com/docs/v2/resources/third-party-libs/
-
-  For more info on type definition files, check out the Typescript docs here:
-  https://www.typescriptlang.org/docs/handbook/declaration-files/introduction.html
-*/
 declare module "*";
 
 declare class Homewatch {
   auth: string;
   axios: any;
-  homes: Homes;
-  users: Users;
+  homes: Homewatch.Homes;
+  users: Homewatch.Users;
   constructor(url: string, auth?: string);
-  scenarioThings(scenario: Object): void;
-  scenarios(home: Object): void;
-  status(thing: Object): ThingStatus;
-  things(home: { id: number }): Things;
+  scenarioThings(scenario: { id: number }): void;
+  scenarios(home: { id: number }): Homewatch.Scenarios;
+  status(thing: { id: number }): Homewatch.ThingStatus;
+  things(home: { id: number }): Homewatch.Things;
 }
 
-declare class Homes {
-  createHome(...args: any[]): any;
-  deleteHome(...args: any[]): any;
-  getHome(...args: any[]): any;
-  listHomes(...args: any[]): any;
-  updateHome(...args: any[]): any;
-}
+declare namespace Homewatch {
+  class Homes {
+    private constructor();
+    createHome(home: { name: string, tunnel: string, location: string }): Promise<any>;
+    deleteHome(id: number): Promise<any>;
+    getHome(id: number): Promise<any>;
+    listHomes(): Promise<any>;
+    updateHome(id: number, home: { name: string, tunnel: string, location: string }): Promise<any>;
+  }
 
-declare class Users {
-  login(auth: Object): any;
-  register(user: Object): any;
-  currentUser(): any;
-  updateCurrentUser(user: Object): any;
-}
+  class Users {
+    private constructor();
+    login(auth: { email: string, password: string }): Promise<any>;
+    register(user: { name: string, email: string, password: string, password_confirmation: string }): Promise<any>;
+    currentUser(): Promise<any>;
+    updateCurrentUser(user: { name: string, email: string, password: string, password_confirmation: string }): Promise<any>;
+  }
 
-declare class Things {
-  listThings(): any;
-  createThing(home: any): any;
-  updateThing(id: number, home: any): any;
-  deleteThing(id: number): any;
-  getThing(id: number): any;
-}
+  class Things {
+    private constructor();
+    listThings(): Promise<any>;
+    createThing(home: any): Promise<any>;
+    updateThing(id: number, home: any): Promise<any>;
+    deleteThing(id: number): Promise<any>;
+    getThing(id: number): Promise<any>;
+  }
 
-declare class ThingStatus {
-  getStatus(): Promise<any>;
-  putStatus(status: Object): Promise<any>;
+  class ThingStatus {
+    private constructor();
+    getStatus(): Promise<any>;
+    putStatus(status: Object): Promise<any>;
+  }
+
+  class Scenarios {
+    private constructor();
+    createScenario(scenario: { name: string }): Promise<any>;
+    deleteScenario(id: number): Promise<any>;
+    getScenario(id: number): Promise<any>;
+    listScenario(): Promise<any>;
+    updateScenario(id: number, scenario: { name: string }): Promise<any>;
+  }
 }
