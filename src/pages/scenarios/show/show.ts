e02be91de605e31b1d6df7307f0817035cd2dc1c
José Sousa <josesousa9000@gmail.com>
2017-06-20 16:47:27 +0100

    Scenario show page

--- src/pages/scenarios/show/show.ts
+++ src/pages/scenarios/show/show.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ThingsInfo } from "../../../services/things_info";
 
 @Component({
   selector: 'page-show',
@@ -8,11 +9,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class ShowScenarioPage {
   scenario: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public thingsInfo: ThingsInfo) {
     this.scenario = this.navParams.get("scenario");
   }
 
-  ionViewDidLoad() {
+  getIconFromType(type: string) {
+    return this.thingsInfo.getThingInfo(type).icon;
+  }
 
+  stringify(status: Object) {
+    return JSON.stringify(status);
   }
 }
